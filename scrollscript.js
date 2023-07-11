b9eff45073fdd27e74ec2cb38a808f55721dd875
Jack Pagano <Pags12189@gmail.com>
2023-07-11 14:42:45 -0400

    scroll button fixed area test

---
+++ scrollscript.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {
+  var scrollBtn = document.getElementById('scrollBtn');
+  var scrollPosition = window.pageYOffset;
+  var scrollLimit = 10 * getLineHeight(); // modify 10
+
+  if (scrollPosition > scrollLimit) {
+    scrollBtn.style.position = 'fixed';
+    scrollBtn.style.bottom = '20px'; // Adjust for button placement
+    scrollBtn.style.right = '20px';
+  } else {
+    scrollBtn.style.position = 'static';
+  }
+}
+
+function getLineHeight() {
+  var temp = document.createElement('div');
+  temp.style['font-size'] = window.getComputedStyle(document.body)['font-size'];
+  temp.innerHTML = 'A';
+  document.body.appendChild(temp);
+  var lineHeight = temp.offsetHeight;
+  document.body.removeChild(temp);
+  return lineHeight;
+}
+
