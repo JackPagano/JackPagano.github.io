7e97bb0a43563a7d5d7a28d2bb23bc09047f5456
Jack Pagano <Pags12189@gmail.com>
2023-07-11 15:41:53 -0400

    more space

--- scrollscript.js
+++ scrollscript.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var scrollButton = document.getElementById('scrollButton');
-    if (window.scrollY >= 400) { // enable button if user scrolls down 20 lines (approx. 400px)
+    if (window.scrollY >= 300) { // enable button if user scrolls down 20 lines (approx. 400px)
       scrollButton.style.display = 'block';
       scrollButton.disabled = false;
     } else { // disable button if user scrolls less than 20 lines
