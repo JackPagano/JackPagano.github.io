b31154452c0b18141982946b983740eec1b7d783
Jack Pagano <Pags12189@gmail.com>
2023-07-11 20:20:31 -0400

    Scroll button fixed

--- scrollscript.js
+++ scrollscript.js
@@ -1,10 +1,13 @@
+scrollButton.style.display = 'none';
+scrollButton.disabled = true;
+
 window.addEventListener('scroll', function() {
     var scrollButton = document.getElementById('scrollButton');
-    if (window.scrollY < 300) { // enable button if user scrolls down 20 lines (approx. 400px)
-      scrollButton.style.display = 'none';
-      scrollButton.disabled = true;
-    } else { // disable button if user scrolls less than 20 lines
+    if (window.scrollY >= 300) { // enable button if user scrolls down 20 lines (approx. 400px)
       scrollButton.style.display = 'block';
       scrollButton.disabled = false;
+    } else { // disable button if user scrolls less than 20 lines
+      scrollButton.style.display = 'none';
+      scrollButton.disabled = true;
     }
 });
