5a302392138ba70c2d5cb028487b6f7aabd85b19
Jack Pagano <Pags12189@gmail.com>
2023-07-13 15:47:10 -0400

    Site 2.0

---
+++ contactme.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("contact").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.open("mailto:Pags12189@gmail.com", "_blank");
+    });
+}
---
+++ scroll.js
@@ -0,0 +1,17 @@
+// disable button initially
+document.addEventListener("DOMContentLoaded", function() {
+  var scrollButton = document.getElementById('scrollButton');
+  scrollButton.style.display = 'none';
+  scrollButton.disabled = true;
+});
+
+window.addEventListener('scroll', function() {
+  var scrollButton = document.getElementById('scrollButton');
+  if (window.scrollY >= 300) { // enable button if user scrolls down 20 lines (approx. 400px)
+    scrollButton.style.display = 'block';
+    scrollButton.disabled = false;
+  } else { // disable button if user scrolls less than 20 lines
+    scrollButton.style.display = 'none';
+    scrollButton.disabled = true;
+  }
+});
