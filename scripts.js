b68c025e18c2d3e50b0d6347eb9c15cd82b3c8a8
Jack Pagano <pags12189@gmail.com>
2023-07-18 17:20:07 -0400

    contact me page

--- scripts.js
+++ scripts.js
@@ -1,14 +1,38 @@
+
+
 // scripts.js
 document.addEventListener("DOMContentLoaded", function() {
   const scrollButton = document.getElementById('scrollButton');
   const switchTheme = document.querySelector('#checkbox');
   const body = document.body;
-  
-  // Contact Me Button
-  document.getElementById("contact").addEventListener("click", function(event) {
-    event.preventDefault();
-    window.open("mailto:Pags12189@gmail.com", "_blank");
+
+  // // Send Email Button (Work in Progress)
+  // document.getElementById("contact-form").addEventListener("submit", function(event) {
+  //   event.preventDefault();
+  //   const email = document.getElementById("email").value;
+  //   const message = document.getElementById("message").value;
+  //   // Perform an AJAX request to the server-side script and pass the email and body data
+  //   // Use a library like Axios or fetch to send the data to a server-side script for email processing
+  // });
+
+  // Current Page Underline
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const activeLink = document.querySelector('.nav-link.active');
+      if (activeLink) {
+        activeLink.classList.remove('active');
+      }
+      this.classList.add('active');
+    });
   });
+
+  const currentPage = window.location.pathname.split('/').pop();
+  const activeLink = document.querySelector(`.nav-link[href="${currentPage}"]`);
+  if (activeLink) {
+    activeLink.classList.add('active');
+  }
   
   // Theme Switch Button
   function toggleTheme() {
@@ -38,12 +62,14 @@ document.addEventListener("DOMContentLoaded", function() {
   if (storedTheme) {
     body.classList.add(storedTheme);
     switchTheme.checked = storedTheme === 'dark-theme';
+  } else {
+    // Set default theme based on system preference
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme';
+    body.classList.add(systemTheme);
+    switchTheme.checked = systemTheme === 'dark-theme';
+    localStorage.setItem('theme', systemTheme);
   }
   
   // Initial update of scroll button visibility
   updateScrollButtonVisibility();
 });
-  
-      
-
-  
